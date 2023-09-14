f95bd6dd98e15f752c6bca357400bba74184dd14
dangannn <dn14072004@mail.ru>
2023-09-14 23:20:16 +0300

    feat: config

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"GoBackend/config"
+	"os"
+)
 
+func main() {
+	config := config.InitConfig(getConfigFileName())
+}
+
+func getConfigFileName() string {
+	env := os.Getenv("ENV")
+
+	if env != "" {
+		return "config-" + env
+	}
+
+	return "config"
 }
